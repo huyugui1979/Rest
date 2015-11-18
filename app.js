22c0febe831e7b252ddd420a2f6ef8e06ec20cb2
huyugui1979 <huyugui1979@126.com>
2015-11-18 21:49:32 +0800

    commit

--- app.js
+++ app.js
@@ -6,26 +6,16 @@ var restify = require('express-restify-mongoose')
 require('./model/model.js')
 var app = express()
 var router = express.Router()
-var routes = require('./routes/index.js')
-app.use(routes);
+
 app.use(bodyParser.json())
 app.use(methodOverride())
-app.use(
-    function crossOrigin(req,res,next){
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      return next();
-    }
-);
 
-//
 mongoose.connect('mongodb://huyugui.eicp.net/restify')
 
-restify.serve(router, mongoose.model('Student'));
-restify.serve(router, mongoose.model('Teacher'));
+restify.serve(router, mongoose.model('Student'))
 
-app.use(routes);
+app.use(router)
 
 app.listen(9999, function () {
-  console.log('Express server listening on port 9999')
+    console.log('Express server listening on port 9999')
 })
